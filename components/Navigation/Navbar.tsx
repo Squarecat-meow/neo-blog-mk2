341d8816876a0627b002a22d801218fa7ad1bbcf
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-04 12:47:05 +0900

    💄feat(components): 컴포넌트 분리

---
+++ components/Navigation/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="w-full lg:w-3/4 text-center space-x-32 py-3 border-b border-black">
+      <button className="font-light hover:font-normal text-2xl">Home</button>
+      <button className="font-light hover:font-normal text-2xl">Post</button>
+      <button className="font-light hover:font-normal text-2xl">About</button>
+    </nav>
+  );
+}
---
+++ components/Navigation/Sidebar.tsx
@@ -0,0 +1,43 @@
+import Image from 'next/image';
+import MockProfile from '../../public/images/mock-profile-picture.webp';
+
+export default function Sidebar() {
+  const mock = {
+    일상: ['├ 여행', '├ 바이크', '└ 음악'],
+    개발: ['├ 리눅스', '├ 연합우주', '├ React/Next', '└ Typescript'],
+  };
+  return (
+    <nav className="w-1/5 sticky top-0 h-fit">
+      <div className="w-full flex flex-col lg:flex-row justify-between items-center space-x-6 py-4 pr-2 border-b border-black">
+        <Image
+          src={MockProfile}
+          alt="프로필사진 목데이터"
+          className="w-20 h-20 rounded-full"
+        />
+        <div>
+          <span className="text-3xl">요즈미나</span>
+          <p className="font-light break-keep">
+            &lt;head&gt;와 &lt;body&gt;로 이루어진 사람
+          </p>
+        </div>
+      </div>
+      <div className="py-4 space-y-2">
+        <p className="text-2xl">카테고리</p>
+        {Object.entries(mock).map(([key, value], index) => (
+          <ul key={index}>
+            <li className="text-lg">
+              {key}
+              <ul>
+                {value.map((el, index) => (
+                  <li key={index} className="font-light hover:font-normal">
+                    {el}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          </ul>
+        ))}
+      </div>
+    </nav>
+  );
+}
