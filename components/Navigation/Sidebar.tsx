a580f5c2c4bd4eaa8d9f17bb5d6ef3d4c79d9314
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-04 20:59:58 +0900

    🔥 사이드바에서 프로필 삭제

--- components/Navigation/Sidebar.tsx
+++ components/Navigation/Sidebar.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image';
-import MockProfile from '../../public/images/mock-profile-picture.webp';
-
 export default function Sidebar() {
   const mock = {
     일상: ['├ 여행', '├ 바이크', '└ 음악'],
@@ -8,19 +5,6 @@ export default function Sidebar() {
   };
   return (
     <nav className="w-1/5 sticky top-0 h-fit">
-      <div className="w-full flex flex-col lg:flex-row justify-between items-center space-x-6 py-6 pr-2 border-b border-black">
-        <Image
-          src={MockProfile}
-          alt="프로필사진 목데이터"
-          className="w-20 h-20 rounded-full"
-        />
-        <div>
-          <span className="text-3xl">요즈미나</span>
-          <p className="font-light break-keep">
-            &lt;head&gt;와 &lt;body&gt;로 이루어진 사람
-          </p>
-        </div>
-      </div>
       <div className="py-4 space-y-4">
         <p className="text-2xl">카테고리</p>
         {Object.entries(mock).map(([key, value], index) => (
