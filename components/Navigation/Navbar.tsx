2d4db7701f3468bb9af08fee0f388726f82a6b25
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-06 03:55:17 +0900

    ✨ feat(Navbar): Navbar에 프로필 아이콘 추가

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -1,15 +1,31 @@
+'use client';
+
+import { CircleUserRound } from 'lucide-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [isHover, setIsHover] = useState(false);
+
   return (
-    <nav className="w-full lg:w-3/4 text-center space-x-44 py-4 border-b border-black">
-      <Link href={'/'}>
-        <button className="font-light hover:font-normal text-2xl">Home</button>
+    <nav className="w-full lg:w-3/4 flex items-center justify-center space-x-44 py-4 border-b border-black">
+      <Link href={'/'} className="font-light hover:font-normal text-2xl">
+        Home
       </Link>
-      <Link href={'/post'}>
-        <button className="font-light hover:font-normal text-2xl">Post</button>
+      <Link href={'/post'} className="font-light hover:font-normal text-2xl">
+        Post
       </Link>
-      <button className="font-light hover:font-normal text-2xl">About</button>
+      <Link href={'/about'} className="font-light hover:font-normal text-2xl">
+        About
+      </Link>
+      <Link href={'/login'} className="h-8">
+        <CircleUserRound
+          size={32}
+          strokeWidth={isHover ? 1 : 0.5}
+          onMouseOver={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        />
+      </Link>
     </nav>
   );
 }
