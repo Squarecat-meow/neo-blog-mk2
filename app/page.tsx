4e66009eee3b33f5829676239cdee3bdc8a43fe6
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-06 03:05:59 +0900

    ✨ feat(Pages): Page에서 DB 쿼리 때리게

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,36 @@
 import PostCard from '@/components/Card/PostCard';
-import { mockPosts } from '@/mock/PostMock';
+import prismaClient from '@/lib/prisma';
+import { IPost } from '@/types/PostTypes';
 
-export default function Home() {
+export default async function Home() {
+  const prisma = prismaClient;
+
+  const posts = (await prisma.post.findMany()) as IPost[];
+
   return (
     <main className="flex">
       <section className="w-3/4">
-        <section className="grid grid-rows-2 border-b divide-y">
-          {mockPosts.slice(0, 2).map((post) => (
-            <div key={post.id} className="p-4 @container">
-              <PostCard post={post} />
-            </div>
-          ))}
-        </section>
-        <section className="grid grid-cols-3 divide-x">
-          {mockPosts.slice(2, 5).map((post) => (
-            <div key={post.id} className="p-4">
-              <PostCard key={post.id} post={post} />
-            </div>
-          ))}
-        </section>
+        {posts.length !== 0 ? (
+          <>
+            <section className="grid grid-rows-2 border-b divide-y">
+              {posts.slice(0, 2).map((post) => (
+                <div key={post.id} className="p-4 @container">
+                  <PostCard post={post} />
+                </div>
+              ))}
+            </section>
+            <section className="grid grid-cols-3 divide-x">
+              {posts.length > 2 &&
+                posts.slice(2, 5).map((post) => (
+                  <div key={post.id} className="p-4">
+                    <PostCard key={post.id} post={post} />
+                  </div>
+                ))}
+            </section>
+          </>
+        ) : (
+          <h1>포스트가 없어요!</h1>
+        )}
       </section>
       <section className="w-1/4 border-l p-6">미스키 넣기</section>
     </main>
--- components/Navigation/Sidebar.tsx
+++ components/Navigation/Sidebar.tsx
@@ -1,26 +1,18 @@
-export default function Sidebar() {
-  const mock = {
-    일상: ['├ 여행', '├ 바이크', '└ 음악'],
-    개발: ['├ 리눅스', '├ 연합우주', '├ React/Next', '└ Typescript'],
-  };
+import prismaClient from '@/lib/prisma';
+
+const prisma = prismaClient;
+
+export default async function Sidebar() {
+  const categories = await prisma.category.findMany({
+    select: {
+      subCategories: true,
+    },
+  });
+
   return (
     <nav className="w-1/5 sticky top-0 h-fit">
       <div className="py-4 space-y-4">
         <p className="text-2xl">카테고리</p>
-        {Object.entries(mock).map(([key, value], index) => (
-          <ul key={index}>
-            <li className="text-lg">
-              {key}
-              <ul>
-                {value.map((el, index) => (
-                  <li key={index} className="font-light hover:font-normal">
-                    {el}
-                  </li>
-                ))}
-              </ul>
-            </li>
-          </ul>
-        ))}
       </div>
     </nav>
   );
