d9f362adf209e7f90ed2d32188121df635a9e632
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-06 22:39:34 +0900

    💄style(ProfileButton): 전반적 스타일 수정 & 조건부 렌더링 구현

--- components/Button/ProfileButton.tsx
+++ components/Button/ProfileButton.tsx
@@ -4,10 +4,10 @@ import { useUserStore } from '@/store/UserStore';
 import { IUser } from '@/types/UserType';
 import { CircleUserRound, Loader2Icon } from 'lucide-react';
 import Link from 'next/link';
+import { DropdownMenu } from 'radix-ui';
 import { useEffect, useState } from 'react';
 
 export function ProfileButton() {
-  const [isHover, setIsHover] = useState(false);
   const [user, setUser] = useState<IUser | null>();
   const { currentUser } = useUserStore();
 
@@ -34,22 +34,43 @@ export function ProfileButton() {
         </Link>
       );
     default:
-      return (
-        <>
-          {user.profileImgUrl ? (
-            <img
-              src={user.profileImgUrl}
-              alt={`${user.nickname}의 프로필 사진`}
-            />
-          ) : (
-            <CircleUserRound
-              strokeWidth={isHover ? 1 : 0.5}
-              size={32}
-              onMouseOver={() => setIsHover(true)}
-              onMouseLeave={() => setIsHover(false)}
-            />
-          )}
-        </>
-      );
+      return <UserChip user={user} />;
   }
+}
+
+function UserChip({ user }: { user: IUser }) {
+  const [isHover, setIsHover] = useState(false);
+  return (
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className="outline-none">
+        {user.profileImgUrl ? (
+          <img
+            src={user.profileImgUrl}
+            alt={`${user.nickname}의 프로필 사진`}
+          />
+        ) : (
+          <CircleUserRound
+            strokeWidth={isHover ? 1 : 0.5}
+            size={32}
+            onMouseOver={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+          />
+        )}
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="font-light border border-black mt-2 p-2 bg-neutral-200">
+          <DropdownMenu.Item className="hover:font-normal text-black outline-none">
+            <Link href={'/writer'}>New Post</Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item className="hover:font-normal text-black outline-none">
+            <Link href={'/mypage'}>My Page</Link>
+          </DropdownMenu.Item>
+          <DropdownMenu.Separator className="border-b border-black my-2" />
+          <DropdownMenu.Item className="hover:font-normal text-black outline-none">
+            <button>Logout</button>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  );
 }
