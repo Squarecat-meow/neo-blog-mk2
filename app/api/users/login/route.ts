5b1c6e0a60b0bc1b154c587b25d8a5e6dc34dbd4
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-06 19:19:44 +0900

    ✨ JWT token 관련 기능 추가

--- app/api/users/login/route.ts
+++ app/api/users/login/route.ts
@@ -2,6 +2,7 @@ import { ILogin } from '@/types/UserType';
 import { NextRequest, NextResponse } from 'next/server';
 import bcrypt from 'bcrypt';
 import prismaClient from '@/lib/prisma';
+import { publishAccessToken, publishRefreshToken } from '@/utils/token';
 
 const prisma = prismaClient;
 
@@ -26,7 +27,40 @@ export async function POST(req: NextRequest) {
 
     if (!validPassword)
       throw new Error('아이디나 비밀번호가 일치하지 않습니다.');
-    else return NextResponse.json({}, { status: 200 });
+
+    const accessToken = await publishAccessToken(data.id);
+    const refreshToken = await publishRefreshToken(data.id);
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        userId: user.userId,
+      },
+      data: {
+        refreshToken: refreshToken,
+      },
+    });
+
+    console.log(updatedUser);
+
+    const response = NextResponse.json({ success: true });
+
+    response.cookies.set('accessToken', accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 15 * 60,
+      path: '/',
+    });
+
+    response.cookies.set('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60,
+      path: '/',
+    });
+
+    return response;
   } catch (err) {
     if (err instanceof Error)
       return NextResponse.json({ error: err.message }, { status: 400 });
---
+++ utils/token.ts
@@ -0,0 +1,33 @@
+import * as jose from 'jose';
+
+export async function publishAccessToken(userId: string) {
+  const secret = jose.base64url.decode(process.env.JWT_SECRET!);
+  const jwt = await new jose.SignJWT({ userId: userId })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setIssuer(process.env.JWT_ISSUER ?? 'http://localhost:3000')
+    .setExpirationTime('15m')
+    .sign(secret);
+
+  return jwt;
+}
+
+export async function publishRefreshToken(userId: string) {
+  const secret = jose.base64url.decode(process.env.JWT_SECRET!);
+  const jwt = await new jose.SignJWT({ userId: userId, type: 'REFRESH' })
+    .setProtectedHeader({ alg: 'HS256' })
+    .setIssuedAt()
+    .setIssuer(process.env.JWT_ISSUER ?? 'http://localhost:3000')
+    .setExpirationTime('7d')
+    .setJti(crypto.randomUUID())
+    .sign(secret);
+
+  return jwt;
+}
+
+export async function verifyToken(token: string) {
+  const secret = jose.base64url.decode(process.env.JWT_SECRET!);
+  const { payload } = await jose.jwtVerify(token, secret);
+
+  return payload;
+}
