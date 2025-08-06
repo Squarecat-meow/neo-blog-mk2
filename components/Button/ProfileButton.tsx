03af60959011fa731129303f04e6e7233f936d69
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-06 22:10:04 +0900

    ✨ feat(ProfileButton): ProfileButton 로그인 여부 확인 로직 작성

--- components/Button/ProfileButton.tsx
+++ components/Button/ProfileButton.tsx
@@ -1,37 +1,55 @@
 'use client';
 
 import { useUserStore } from '@/store/UserStore';
-import { CircleUserRound } from 'lucide-react';
+import { IUser } from '@/types/UserType';
+import { CircleUserRound, Loader2Icon } from 'lucide-react';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function ProfileButton() {
   const [isHover, setIsHover] = useState(false);
+  const [user, setUser] = useState<IUser | null>();
   const { currentUser } = useUserStore();
 
-  if (currentUser) {
-    return (
-      <div>
-        {currentUser.profileImgUrl ? (
-          <img
-            src={currentUser.profileImgUrl}
-            alt="로그인 한 사용자 프로필 사진"
-          />
-        ) : (
-          <CircleUserRound
-            size={32}
-            strokeWidth={isHover ? 1 : 0.5}
-            onMouseOver={() => setIsHover(true)}
-            onMouseLeave={() => setIsHover(false)}
-          />
-        )}
-      </div>
-    );
-  } else {
-    return (
-      <Link href={'/login'} className="h-8">
-        Login
-      </Link>
-    );
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await fetch('/api/users/me');
+      const data = await res.json();
+
+      if (data.authenticated) setUser(data.currentUser);
+      else setUser(null);
+    };
+
+    if (currentUser) setUser(currentUser);
+    else fetchUser();
+  }, [currentUser]);
+
+  switch (user) {
+    case undefined:
+      return <Loader2Icon className="animate-spin" />;
+    case null:
+      return (
+        <Link href={'/login'} className="text-2xl font-light hover:font-normal">
+          Login
+        </Link>
+      );
+    default:
+      return (
+        <>
+          {user.profileImgUrl ? (
+            <img
+              src={user.profileImgUrl}
+              alt={`${user.nickname}의 프로필 사진`}
+            />
+          ) : (
+            <CircleUserRound
+              strokeWidth={isHover ? 1 : 0.5}
+              size={32}
+              onMouseOver={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+            />
+          )}
+        </>
+      );
   }
 }
