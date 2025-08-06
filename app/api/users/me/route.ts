c4b1ab722f7990430c1eab28a724befa826e6afc
Squarecat-meow <Yozumina@serafuku.moe>
2025-08-06 22:09:30 +0900

    ✨ feat(API): /api/users/me 엔드포인트 작성

---
+++ app/api/users/me/route.ts
@@ -0,0 +1,39 @@
+import prismaClient from '@/lib/prisma';
+import { IAccessTokenPayload } from '@/types/TokenType';
+import { verifyToken } from '@/utils/token';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+const prisma = prismaClient;
+
+export async function GET() {
+  try {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('accessToken');
+    if (!accessToken) {
+      return NextResponse.json({ authenticated: false }, { status: 200 });
+    }
+
+    const payload = (await verifyToken(
+      accessToken.value,
+    )) as IAccessTokenPayload;
+
+    const foundedUser = await prisma.user.findUnique({
+      where: {
+        userId: payload.userId,
+      },
+      select: {
+        id: true,
+        userId: true,
+        nickname: true,
+        introduction: true,
+        profileImgUrl: true,
+      },
+    });
+
+    return NextResponse.json({ authenticated: true, currentUser: foundedUser });
+  } catch (err) {
+    if (err instanceof Error)
+      return NextResponse.json({ error: err.message }, { status: 400 });
+  }
+}
