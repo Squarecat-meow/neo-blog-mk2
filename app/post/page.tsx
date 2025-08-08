2cd94eb00c72af2625bc7c14a7943f932ea2d189
Squarecat-meow <yozumina@serafuku.moe>
2025-08-09 06:13:17 +0900

    🔥 posts 페이지에 목데이터 삭제

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,14 +1,3 @@
-import PostCard from '@/components/Card/PostCard';
-import { mockPosts } from '@/mock/PostMock';
-
 export default function Page() {
-  return (
-    <>
-      {mockPosts.map((post) => (
-        <div key={post.id} className="p-8">
-          <PostCard post={post} />
-        </div>
-      ))}
-    </>
-  );
+  return <></>;
 }
