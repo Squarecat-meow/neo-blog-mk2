8b51d97f481807c958e486a2d99c19aeb6c8d51f
Squarecat-meow <yozumina@serafuku.moe>
2025-08-09 10:04:19 +0900

    💄 style(Navbar): Hover 이펙트 제거

--- components/Navigation/Navbar.tsx
+++ components/Navigation/Navbar.tsx
@@ -6,16 +6,16 @@ import Logo from '@/public/images/logo.gif';
 export default function Navbar() {
   return (
     <nav className="w-full lg:w-3/4 flex items-center justify-center gap-32 py-2">
-      <Link href="/post" className="font-light hover:font-normal text-xl">
+      <Link href="/post" className="text-xl font-light">
         Post
       </Link>
-      <Link href="/about" className="font-light hover:font-normal text-xl">
+      <Link href="/about" className="text-xl font-light">
         About
       </Link>
-      <Link href="/" className="font-light hover:font-normal text-xl">
+      <Link href="/" className="text-xl font-light">
         <Image src={Logo} alt="블로그 로고" width={120} height={30} />
       </Link>
-      <Link href="/timeline" className="font-light hover:font-normal text-xl">
+      <Link href="/timeline" className="text-xl font-light">
         Timeline
       </Link>
       <ProfileButton />
