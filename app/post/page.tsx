ddbbe51ff6f50a074cfe23999fc131d38b4f4afe
Squarecat-meow <yozumina@serafuku.moe>
2025-08-11 00:27:34 +0900

    💄 style(PostListItem): 포스트 페이지 리스트 컴포넌트 작성

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,3 +1,13 @@
+import PostListItem from '@/components/Primitives/Card/PostListItem';
+import { mockPosts } from '@/mock/mockdata';
+
 export default function Page() {
-  return <></>;
+  const posts = mockPosts;
+  return (
+    <section className="flex flex-col gap-4">
+      {posts.map((el) => (
+        <PostListItem post={el} key={el.id} />
+      ))}
+    </section>
+  );
 }
---
+++ components/Primitives/Card/PostListItem.tsx
@@ -0,0 +1,33 @@
+import { IPost } from '@/types/PostTypes';
+import dateParser from '@/utils/dateParser';
+import Link from 'next/link';
+
+export default function PostListItem({ post }: { post: IPost }) {
+  return (
+    <Link
+      href={`/post/${post.id}`}
+      className="h-32 flex gap-2 border border-slate-200 rounded-lg transition-shadow hover:shadow-lg"
+    >
+      {post.thumbnailImgUrl && (
+        <img
+          src={post.thumbnailImgUrl}
+          alt="포스트 사진의 썸네일"
+          className="h-32 object-cover aspect-square rounded-l-lg"
+        />
+      )}
+      <div className="p-2">
+        <h1 className="text-2xl font-noto-serif">{post.title}</h1>
+        <div className="flex gap-2">
+          <span className="text-sm font-light">
+            {post.updatedAt
+              ? dateParser(post.updatedAt)
+              : dateParser(post.createdAt)}
+          </span>
+          <span className="text-sm font-light">|</span>
+          <span className="text-sm font-light">{post.categoryName}</span>
+        </div>
+        <p className="text-lg">{post.body}</p>
+      </div>
+    </Link>
+  );
+}
