a5b441d5b0f05fb16f02adb469c717504b7301d8
Squarecat-meow <yozumina@serafuku.moe>
2025-08-15 15:32:06 +0900

    ✨ feat(NewCategoryModal): 새 카테고리 모달 작성

--- components/Writer/NewCategoryModal.tsx
+++ components/Writer/NewCategoryModal.tsx
@@ -1,7 +1,14 @@
+'use client';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
 import Button from '../Primitives/Button/Button';
 import Input from '../Primitives/Input/Input';
 import Label from '../Primitives/Label/Label';
 import Modal from '../Primitives/Modal/Modal';
+
+interface INewCategory {
+  categoryName: string;
+}
 
 export default function NewCategoryModal({
   isVisible,
@@ -10,15 +17,27 @@ export default function NewCategoryModal({
   isVisible: boolean;
   setIsVisible: (state: boolean) => void;
 }) {
+  const { register, handleSubmit } = useForm<INewCategory>();
+
+  const onSubmit: SubmitHandler<INewCategory> = async (e) => {
+    fetch('/api/writer/new-category', {
+      method: 'POST',
+      body: JSON.stringify(e.categoryName),
+    });
+    setIsVisible(false);
+  };
   return (
     <Modal modalVisibleState={isVisible} setModalVisibleState={setIsVisible}>
-      <section className="p-2">
-        <form>
+      <section className="p-4">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <Label label="새 카테고리 이름">
-            <Input placeholder="이름을 입력해 주세요" />
+            <Input
+              placeholder="이름을 입력해 주세요"
+              {...register('categoryName')}
+            />
           </Label>
           <div className="w-full text-right">
-            <Button variant="primary" className="mt-2 px-4">
+            <Button variant="primary" className="mt-4 px-6">
               확인
             </Button>
           </div>
