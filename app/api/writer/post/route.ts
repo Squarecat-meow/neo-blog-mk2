496fc75ca4a2bb8d9b06ea5bbd1742862db56e9b
Squarecat-meow <yozumina@serafuku.moe>
2025-08-18 13:45:28 +0900

    ✨ feat(post): Post에 POST할 때(ㅋㅋ) accessToken 필요하게

--- app/api/writer/post/route.ts
+++ app/api/writer/post/route.ts
@@ -1,7 +1,17 @@
 import prismaClient from '@/lib/prisma';
+import { verifyToken } from '@/utils/token';
+import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+
+  if (!accessToken)
+    return NextResponse.redirect(`${process.env.APP_URL}/api/users/refresh`);
+
+  const { userId } = await verifyToken(accessToken);
+
   const data = await req.json();
 
   const category = await prismaClient.category.findUnique({
@@ -18,6 +28,7 @@ export async function POST(req: NextRequest) {
 
   const result = await prismaClient.post.create({
     data: {
+      authorId: userId,
       title: data.title,
       body: data.body,
       categoryName: category.name,
