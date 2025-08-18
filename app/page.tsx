2d797af26a83cfea248359277bdfc88588905e3a
Squarecat-meow <yozumina@serafuku.moe>
2025-08-18 13:46:23 +0900

    🔥 fix(mockdata): 목데이터 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import GithubHeatmap from '@/components/Heatmap/Heatmap';
 import PostCard from '@/components/Primitives/Card/PostCard';
 import prismaClient from '@/lib/prisma';
-import { mockPosts } from '@/mock/mockdata';
 
 export default async function Home() {
   const prisma = prismaClient;
 
-  // const posts = await prisma.post.findMany();
-  const posts = mockPosts;
+  const posts = await prisma.post.findMany();
+  const postList = posts.slice(0, 6);
 
   return (
     <section className="w-full space-y-6 mt-2">
@@ -16,8 +15,8 @@ export default async function Home() {
         <GithubHeatmap username="yunochi" />
       </article>
       <article className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
-        {posts.length !== 0 ? (
-          posts.map((el) => <PostCard key={el.id} post={el} />)
+        {postList.length !== 0 ? (
+          postList.map((el) => <PostCard key={el.id} post={el} />)
         ) : (
           <h1>포스트가 없어요!</h1>
         )}
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,5 +1,7 @@
 import prismaClient from '@/lib/prisma';
 import dateParser from '@/utils/dateParser';
+import Image from 'next/image';
+import ErrorImage from '@/public/images/error.webp';
 
 export default async function Page({
   params,
@@ -15,20 +17,45 @@ export default async function Page({
       },
     });
 
+    const user = await prismaClient.user.findUniqueOrThrow({
+      where: {
+        userId: post.authorId,
+      },
+    });
+
+    // TODO: markdown 파싱하기
     return (
       <article className="w-5/6 flex flex-col mt-6">
         <h1 className="text-5xl font-noto-serif text-center mb-4">
           {post.title}
         </h1>
-        <div className="w-full mb-12 text-sm text-center font-light space-x-2">
+        <div className="w-full mb-2 text-sm text-center font-light space-x-2">
           <span>{dateParser(post.createdAt)}</span>
           <span>|</span>
           <span>{post.categoryName}</span>
+        </div>
+        <div className="w-full mb-12 flex gap-2 justify-center items-center text-sm font-light">
+          {user.profileImgUrl ? (
+            <img
+              src={user.profileImgUrl}
+              alt={`${user.nickname}의 프로필 사진`}
+              className="h-8 aspect-square rounded-full"
+            />
+          ) : (
+            <div className="h-8 aspect-square rounded-full bg-gray-200" />
+          )}
+          <span>{user.nickname}</span>
         </div>
         <p>{post.body}</p>
       </article>
     );
   } catch (err) {
-    console.error(err);
+    if (err instanceof Error)
+      return (
+        <article>
+          <Image src={ErrorImage} alt="에러 이미지" />
+          <span>{err.message}</span>
+        </article>
+      );
   }
 }
