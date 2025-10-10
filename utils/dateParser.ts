38e48dac3be69812164dfe7f18b617ce57aaec09
Squarecat-meow <yozumina@serafuku.moe>
2025-10-11 07:12:26 +0900

    fix(dateParser): 한자릿수 앞에 0붙게 수정

--- utils/dateParser.ts
+++ utils/dateParser.ts
@@ -2,8 +2,8 @@ export default function dateParser(date: Date) {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const hour = date.getHours();
-  const minute = date.getMinutes();
+  const hour = ('0' + date.getHours().toString()).slice(-2);
+  const minute = ('0' + date.getMinutes().toString()).slice(-2);
 
   return `${year}년 ${month}월 ${day}일 ${hour}:${minute}`;
 }
