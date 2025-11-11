011c352bef619ebb0267a743e612e648614895d0
Squarecat-meow <yozumina@serafuku.moe>
2025-11-11 10:58:53 +0900

    fix(MarkdownRenderer): 마크다운 렌더러 blockquote, table 렌더링 수정

--- components/Post/MarkdownRenderer.tsx
+++ components/Post/MarkdownRenderer.tsx
@@ -4,6 +4,7 @@ import rehypePrism from 'rehype-prism-plus';
 import 'prismjs/themes/prism-tomorrow.css';
 import OpenGraphCard from './OpenGraphCard';
 import { Text } from '@radix-ui/themes';
+import Image from 'next/image';
 
 export default function MarkdownRenderer({ children }: { children: string }) {
   return (
@@ -30,6 +31,33 @@ export default function MarkdownRenderer({ children }: { children: string }) {
             <Text as="p" mt="2" className="text-sm md:text-md lg:text-lg">
               {children}
             </Text>
+          ),
+          blockquote: ({ children }) => (
+            <blockquote className=" border-l-4 border-l-gray-200">
+              {children}
+            </blockquote>
+          ),
+          table: ({ node, ...props }) => (
+            <table
+              className="border-collapse border border-gray-300"
+              {...props}
+            />
+          ),
+          thead: ({ node, ...props }) => (
+            <thead className="bg-gray-100" {...props} />
+          ),
+          tbody: ({ node, ...props }) => <tbody {...props} />,
+          tr: ({ node, ...props }) => (
+            <tr className="border-b border-gray-300" {...props} />
+          ),
+          th: ({ node, ...props }) => (
+            <th
+              className="border border-gray-300 px-4 py-2 font-semibold"
+              {...props}
+            />
+          ),
+          td: ({ node, ...props }) => (
+            <td className="border border-gray-300 px-4 py-2" {...props} />
           ),
         }}
       >
